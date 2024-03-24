5f5dd366eeb6c4b896aa7d45f5d3712a86e535a9
Richard Williams <rkwilli20204@gmail.com>
2024-03-24 19:11:56 -0400

    Extra commit
    I desperately need merged!

---
+++ controllers/viewDoctors.js
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ * controllers/auth.js - Contains all logic for logging in
+ * 
+ * @author Richard Williams
+ */
+
+const { response } = require("express");
+const { query } = require("../db/dbService");
+
+const viewDoctors = async (req, res = response) => {
+    const { column } = req.body
+    column = column.toUppercase();
+
+    const result = await query('SELECT ' + column + ' FROM Doctor');
+
+    res.json(result);
+
+    // res.json(result);
+};
+
+module.exports = {
+    viewDoctors
+};
