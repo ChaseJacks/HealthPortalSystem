646defc91d7ce592ceb67668695eb7a039821fbf
Richard Williams <rkwilli20204@gmail.com>
2024-03-26 00:01:45 -0400

    Edit the controller to make it send the doctor name instead of the ID

--- controllers/viewAppointments.js
+++ controllers/viewAppointments.js
@@ -5,7 +5,15 @@ const viewAppointments = async (req, res = response) => {
     const { column } = req.body
     column = column.toUppercase();
 
-    const result = await query('SELECT * FROM Appointment WHERE PatientID =' + column + ';');
+    let result = await query('SELECT * FROM Appointment WHERE PatientID =' + column + ';');
+
+    for (let i = 0; i < result.recordset; i++) {
+        let doctorID = result.recordset[i].DoctorID;
+        // Get the name of the doctor
+        let docQuery = await query('SELECT Name FROM Doctor WHERE DoctorID =' + doctorID);
+        let docName = docQuery.recordset[0].Name;
+        result.recordset[i].DoctorID = docName;
+    }
 
     res.json(result);
 
