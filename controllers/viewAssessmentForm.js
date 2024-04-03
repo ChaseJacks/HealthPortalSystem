b8de13e8e3d31c3a8e6ccb2eb4177e36221e38d7
ChaseJacks <157538546+ChaseJacks@users.noreply.github.com>
2024-04-03 01:22:57 -0400

    Revert "Someone fix the viewAssessmentForm API, it's not gonna be me"
    This reverts commit 127804a11562d68334f1227b62d09e641f39f5be.

--- controllers/viewAssessmentForm.js
+++ controllers/viewAssessmentForm.js
@@ -14,8 +14,8 @@ const viewAssessmentForm = async (req, res = response) => {
 
     try {
         const patientID = req.params["patientID"];
-        const result = await query(`SELECT * FROM PatientAssessmentForm WHERE PatientID='${patientID}'`);
-        res.json(JSON.stringify(result.recordset[0]));
+        const result = await query(`SELECT * FROM PatientAssessmentForm WHERE PatientID="${patientID}"`);
+        res.json(result.recordset[0]);
     } catch (err) {
         console.log("Error - " + err.message);
         res.status(500).json({msg: "Error retrieving patient assessment form"});
--- controllers/viewPatients.js
+++ controllers/viewPatients.js
@@ -7,7 +7,7 @@ const viewPatients = async (req, res = response) => {
         
         // Query to select appointments for the current doctor
         const result = await query(`
-            SELECT A.PatientID, A.Date, A.Location, P.Name AS PatientName
+            SELECT A.Date, A.Location, P.Name AS PatientName
             FROM Appointment A
             JOIN Patient P ON A.PatientID = P.PatientID
             WHERE A.DoctorID = (CONVERT(uniqueidentifier, '${doctorID}'))
@@ -17,8 +17,7 @@ const viewPatients = async (req, res = response) => {
         const formattedResult = result.recordset.map(appointment => ({
             Date: appointment.Date,
             Location: appointment.Location,
-            PatientName: appointment.PatientName,
-            PatientID: appointment.PatientID
+            PatientName: appointment.PatientName
         }));
         
         res.json(formattedResult);
